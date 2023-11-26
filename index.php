0ddc49f5ae4f9e766fe2d23ca7d0ac819e815a55
K-Antillon <kathleen.antillon@utp.ac.pa>
2023-11-26 18:26:56 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Bienvenida</title>
+</head>
+<body>
+    <h1>BIENVENIDO</h1>
+    <br>
+    <a href="login.php">Iniciar Sesión</a>
+    <br>
+    <a href="registro.php">Registrarse</a>
+    <br>
+    <a href="lexus.php">Lexus</a>
+    <br>
+    <a href="jeep.php">Jeep</a>
+    <br>
+    <a href="mercedes.php">Mercedes</a>
+    
+</body>
+</html>
---
+++ jeep.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeep</title>
+</head>
+<body>
+    <h1>JEEP</h1>
+    <br>
+
+    <!-- Contenedor para mostrar los datos de la API -->
+    <div id="datos-api"></div>
+
+    <!-- Script para cargar y mostrar los datos de la API -->
+    <script>
+        // Hacer la solicitud a la API
+        fetch('api_jeep.php')
+            .then(response => response.json())
+            .then(data => {
+                // Manipular los datos y mostrarlos en el contenedor
+                const contenedorDatos = document.getElementById('datos-api');
+
+                if (data.length > 0) {
+                    data.forEach(carro => {
+                        const divCarro = document.createElement('div');
+                        divCarro.innerHTML = `
+                            <h2>${carro.nombre}</h2>
+                            <h3>${carro.marca}</h3>
+                            <p>${carro.descripcion}</p>
+                            <img height="200px" src="data:image/jpg;base64,${carro.imagen}">
+                        `;
+                        contenedorDatos.appendChild(divCarro);
+                    });
+                } else {
+                    contenedorDatos.innerHTML = '<p>No hay registros</p>';
+                }
+            })
+            .catch(error => {
+                console.error('Error al obtener datos de la API:', error);
+            });
+    </script>
+</body>
+</html>
